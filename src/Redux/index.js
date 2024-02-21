7ff1a52455276dbc526c79c943022fe4f2c15788
ozo-vehe <johnnie.vehe@gmail.com>
2024-02-22 00:54:27 +0100

    removed redundant files

--- src/Redux/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./auth/reducer";
-import bookingReducer from "./booking/reducer";
-export const rootReducer = combineReducers({
-  auth: authReducer,
-  booking: bookingReducer,
-});
